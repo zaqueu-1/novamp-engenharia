f96c93ba5fc50f726ba396b60ae11efcaf1c11d7
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2023-05-16 07:48:30 -0300

    links-page adjust

--- pages/links/index.jsx
+++ pages/links/index.jsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { RiWhatsappLine } from 'react-icons/ri'
 import { TbBrowserCheck } from 'react-icons/tb'
-import Footer from '../components/Footer'
 
 function index() {
   return (
